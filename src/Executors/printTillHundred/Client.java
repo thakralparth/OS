5401dd3a65e0c2beb59299755c75faeee296a76c
Parth Thakral <parth.thakral72@gmail.com>
2023-03-24 01:00:35 +0530

    executors fns and shutdown

--- src/Executors/printTillHundred/Client.java
+++ src/Executors/printTillHundred/Client.java
@@ -6,15 +6,22 @@ import java.util.concurrent.Executors;
 public class Client {
     public static void main(String[] args) {
         ExecutorService exec_service = Executors.newFixedThreadPool(10);
+//         ExecutorService exec_service = Executors.newSingleThreadExecutor();
+//        ExecutorService exec_service = Executors.newCachedThreadPool();
+
 
         for(int i=1;i<=100;i++){
 
-
+            if(i==5 || i==11 || i==20){
+                System.out.println("Debug");
+            }
 
             printNumber pn = new printNumber(i);
             exec_service.execute(pn);
 //            Thread t=new Thread(pn);
 //            t.start();
         }
+
+        exec_service.shutdown();
     }
 }
